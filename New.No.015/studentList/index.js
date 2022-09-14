773f69e6e1aad32546577a71d1203f68bdddce62
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-09-14 14:35:50 +0900

    15일차 동적 테이블 추가

--- New.No.015/studentList/index.js
+++ New.No.015/studentList/index.js
@@ -11,11 +11,20 @@ const studentsList = [];
 function createStudent(name, age, area, mbti, bloodType) {
   studentsList.push({
     name,
-    age,
     area,
+    age,
     mbti,
     bloodType,
   });
+  // 객체 정의 시 객체 내에 다른 변수만을 넣으면 변수명과 변수의 값을 키와 값에 입력한다.
+  // name 매개변수에 정의된 값을 객체의 name 키에 대한 값으로 정의한다.
+  // 간단 예제
+  let temp = 100;
+  const tempObj = {
+    temp,
+    // temp : temp, << 24번 줄과 같다.
+  };
+  console.log(tempObj);
 }
 
 createStudent("김성진", 27, "성남", "INTP", "B");
@@ -32,6 +41,7 @@ headList.forEach((item) => {
 });
 
 const studentListElem = document.getElementById("data-list");
+
 studentsList.forEach((item, index) => {
   let tempStr = "<tr>";
   headList.forEach((headItem) => {
@@ -41,3 +51,15 @@ studentsList.forEach((item, index) => {
   tempStr += "</tr>";
   studentListElem.innerHTML += tempStr;
 });
+
+// 팀장급 : 아래 코드분석 feat.정현
+// let count = 0;
+// studentListElem.innerHTML = studentsList.reduce((next, curr) => {
+//   return (
+//     next +
+//     `<tr>${headList.reduce((headNext, headCurr) => {
+//       if (headCurr.type === "number") return headNext + `<th>${++count}</th>`;
+//       else return headNext + `<td>${curr[headCurr.type]}</td>`;
+//     }, "")}</tr>`
+//   );
+// }, "");
