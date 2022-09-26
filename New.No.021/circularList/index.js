e40d4459d219fe5c914b727014c328d0bea4fafb
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-09-26 16:26:32 +0900

    트리 추가

--- New.No.021/circularList/index.js
+++ New.No.021/circularList/index.js
@@ -35,6 +35,7 @@ CircularLinkedList.prototype.remove = function (data) {
       const tempNode = curr.next;
       curr.next = tempNode.next;
       if (tempNode === this.tail) this.tail = curr;
+      //   if (curr.next == this.head) this.tail = curr;
       --this.size;
       return curr.data;
     }
@@ -42,4 +43,29 @@ CircularLinkedList.prototype.remove = function (data) {
   }
 };
 
+CircularLinkedList.prototype.toString = function () {
+  if (!this.head) return "";
+  // head가 없으면 빈 문자열 반환
+  let curr = this.head;
+  let tempStr = `${this.head.data}`;
+  // 임시 문자열에 head의 data 정의
+  while (curr !== this.tail) {
+    // curr가 tail이 아니면 실행
+    tempStr += ",";
+    // head의 data가 정의되었으니 한번 끊도록 , 를 추가
+    tempStr += curr.next.data;
+    // 임시 문자열에 다음 Node의 data를 추가
+    curr = curr.next;
+    // 현재 노드를 다음 노드로 정의
+  }
+  return tempStr;
+  // 모든 문자열이 모인 tempStr을 반환
+};
+
 const testCircular = new CircularLinkedList();
+testCircular.insert(1);
+testCircular.insert(2);
+testCircular.insert(3);
+testCircular.insert(4);
+testCircular.insert(5);
+console.log(testCircular.toString()); // "1,2,3,4,5"
