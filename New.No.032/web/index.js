4d848b54fa349649164d40b8fe643ad30309dc3e
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-10-21 11:46:26 +0900

    32일차 3시간

--- New.No.032/web/index.js
+++ New.No.032/web/index.js
@@ -18,13 +18,65 @@ document.getElementById("board-add").onsubmit = function (e) {
 // form 안에 있는 button은 기본적으로 form의 submit을 실행
 
 const tempData = [
-  { title: "arvserv1", text: "9baresrsearvstb" },
-  { title: "arvserv2", text: "8baresrsearvstb" },
-  { title: "arvserv3", text: "7baresrsearvstb" },
-  { title: "arvserv4", text: "6baresrsearvstb" },
-  { title: "arvserv5", text: "5baresrsearvstb" },
-  { title: "arvserv6", text: "4baresrsearvstb" },
-  { title: "arvserv7", text: "3baresrsearvstb" },
-  { title: "arvserv8", text: "2baresrsearvstb" },
-  { title: "arvserv9", text: "1baresrsearvstb" },
+  [
+    { title: "arvserv1", text: "9baresrsearvstb" },
+    { title: "arvserv2", text: "8baresrsearvstb" },
+    { title: "arvserv3", text: "7baresrsearvstb" },
+    { title: "arvserv4", text: "6baresrsearvstb" },
+    { title: "arvserv5", text: "5baresrsearvstb" },
+  ],
+  [
+    { title: "arvserv6", text: "4baresrsearvstb" },
+    { title: "arvserv7", text: "3baresrsearvstb" },
+    { title: "arvserv8", text: "2baresrsearvstb" },
+    { title: "arvserv9", text: "1baresrsearvstb" },
+  ],
 ];
+
+let maxCount = 2; // 총 페이지 수
+let count = 0; // 현재 페이지
+
+const pageElem = document.getElementById("page");
+
+for (let i = 0; i < maxCount; ++i) {
+  const tempLi = document.createElement("li");
+  tempLi.innerText = i + 1;
+  tempLi.onclick = function (e) {
+    count = i;
+    getList();
+  };
+  pageElem.append(tempLi);
+}
+
+const listElem = document.getElementById("list");
+
+function getList() {
+  listElem.innerHTML = "";
+  tempData[count].forEach((data) => {
+    const tempLi = document.createElement("li");
+    const tempTitle = document.createElement("div");
+    const tempH3 = document.createElement("h3");
+    const tempImg = document.createElement("img");
+    const tempText = document.createElement("div");
+    const tempP = document.createElement("p");
+
+    tempTitle.classList.add("title");
+    tempTitle.onclick = function (e) {
+      tempText.classList.toggle("on");
+      tempImg.classList.toggle("on");
+    };
+    tempText.classList.add("text");
+    tempImg.src = "./imgs/angle-up-solid.svg";
+    tempImg.alt = "list-item-btn";
+    tempH3.innerText = data.title;
+    tempP.innerText = data.text;
+
+    tempTitle.append(tempH3);
+    tempTitle.append(tempImg);
+    tempText.append(tempP);
+    tempLi.append(tempTitle);
+    tempLi.append(tempText);
+    listElem.append(tempLi);
+  });
+}
+getList();
