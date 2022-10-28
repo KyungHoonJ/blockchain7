34c34b2efc9de9a2c5cc317e54370be077fae48c
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-10-28 17:17:13 +0900

    36일차 추가

--- New.No.036/public/index.js
+++ New.No.036/public/index.js
@@ -1,49 +1,3 @@
-document.forms["sign-up"].onsubmit = async function (e) {
-  e.preventDefault();
-  if (
-    !e.target["user-id"].value ||
-    !e.target["user-pw"].value ||
-    !e.target["user-name"].value ||
-    !e.target["user-class"].value
-  )
-    return;
-  try {
-    await axios.post("/api/user/regist", {
-      id: e.target["user-id"].value,
-      pw: e.target["user-pw"].value,
-      name: e.target["user-name"].value,
-      className: e.target["user-class"].value,
-    });
-  } catch (error) {
-    console.error(error.response.data.message);
-  }
-};
-
-document.forms["sign-in"].onsubmit = async function (e) {
-  e.preventDefault();
-  if (!e.target["user-id"].value || !e.target["user-pw"].value) return;
-
-  try {
-    const result = await axios.post("/api/user/login", {
-      id: e.target["user-id"].value,
-      pw: e.target["user-pw"].value,
-    });
-    console.log(result.data);
-  } catch (error) {
-    console.error(error.response.data.message);
-  }
-};
-
-document.getElementById("sign-out-btn").onclick = async function (e) {
-  try {
-    await axios.get("/api/user/logout");
-  } catch (error) {
-    console.error(error.response.data.message);
-  }
-};
-
-const boardList = document.getElementById("board-list");
-
 async function getList() {
   try {
     const result = (await axios.get("/api/board")).data;
@@ -111,6 +65,54 @@ async function getList() {
   }
 }
 getList();
+
+document.forms["sign-up"].onsubmit = async function (e) {
+  e.preventDefault();
+  if (
+    !e.target["user-id"].value ||
+    !e.target["user-pw"].value ||
+    !e.target["user-name"].value ||
+    !e.target["user-class"].value
+  )
+    return;
+  try {
+    await axios.post("/api/user/regist", {
+      id: e.target["user-id"].value,
+      pw: e.target["user-pw"].value,
+      name: e.target["user-name"].value,
+      className: e.target["user-class"].value,
+    });
+  } catch (error) {
+    console.error(error.response.data.message);
+  }
+};
+
+document.forms["sign-in"].onsubmit = async function (e) {
+  e.preventDefault();
+  if (!e.target["user-id"].value || !e.target["user-pw"].value) return;
+
+  try {
+    const result = await axios.post("/api/user/login", {
+      id: e.target["user-id"].value,
+      pw: e.target["user-pw"].value,
+    });
+    console.log(result.data);
+    getList();
+  } catch (error) {
+    console.error(error.response.data.message);
+  }
+};
+
+document.getElementById("sign-out-btn").onclick = async function (e) {
+  try {
+    await axios.get("/api/user/logout");
+    getList();
+  } catch (error) {
+    console.error(error.response.data.message);
+  }
+};
+
+const boardList = document.getElementById("board-list");
 
 document.forms["board-add"].onsubmit = async function (e) {
   e.preventDefault();
--- New.No.036/routes/board.js
+++ New.No.036/routes/board.js
@@ -4,15 +4,46 @@ const router = require("express").Router();
 
 const { User, Board } = require("../models/index.js");
 
+router.use("/", (req, res, next) => {
+  global.userId = "";
+  try {
+    const tempUserInfo = jwt.verify(req.cookies.sid, process.env.JWT_KEY);
+    global.userId = tempUserInfo.id;
+    res.cookie(
+      "sid",
+      jwt.sign(
+        {
+          id: tempUserInfo.id,
+          name: tempUserInfo.name,
+        },
+        process.env.JWT_KEY,
+        {
+          algorithm: "HS256",
+          expiresIn: "30m",
+          issuer: "jkh",
+        }
+      )
+    );
+    next();
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 router.get("/", async (req, res) => {
-  const tempBoard = await Board.findAll();
+  const tempBoard = await Board.findAll({
+    // attributes << 찾아봐
+    order: [["id", "DESC"]], // 정렬
+    limit: 5, // 최대 개수
+    offset: 3, // 시작 위치
+  });
   res.send({ list: tempBoard });
 });
 
 router.post("/add", async (req, res) => {
   const tempUser = await User.findOne({
     where: {
-      id: jwt.verify(req.cookies.sid, process.env.JWT_KEY).id,
+      id: global.userId,
     },
   });
   const tempBoard = await Board.create(req.body);
@@ -20,7 +51,17 @@ router.post("/add", async (req, res) => {
   res.end();
 });
 
-router.put("/update", (req, res) => {
+router.put("/update", async (req, res) => {
+  await Board.update(
+    {
+      text: req.body.text,
+    },
+    {
+      where: {
+        id: req.body.id,
+      },
+    }
+  );
   res.end();
 });
 
