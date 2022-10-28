a02f0fe421ac65bb9f85bb3ab89911d461a8ec48
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-10-28 12:15:32 +0900

    36일차 추가

--- New.No.036/public/index.js
+++ New.No.036/public/index.js
@@ -8,14 +8,30 @@ document.forms["sign-up"].onsubmit = async function (e) {
   )
     return;
   try {
-    const result = await axios.post("/api/user/regist", {
+    await axios.post("/api/user/regist", {
       id: e.target["user-id"].value,
       pw: e.target["user-pw"].value,
       name: e.target["user-name"].value,
-      class: e.target["user-class"].value,
+      className: e.target["user-class"].value,
     });
-    if (result.status !== 200) alert("회원가입 오류");
   } catch (error) {
-    console.error(error);
+    console.error(error.response.data.message);
   }
 };
+
+document.forms["sign-in"].onsubmit = async function (e) {
+  e.preventDefault();
+  if (!e.target["user-id"].value || !e.target["user-pw"].value) return;
+
+  try {
+    const result = await axios.post("/api/user/login", {
+      id: e.target["user-id"].value,
+      pw: e.target["user-pw"].value,
+    });
+    console.log(result.data);
+  } catch (error) {
+    console.error(error.response.data.message);
+  }
+};
+
+document.getElementById("sign-out-btn").onclick = function (e) {};
--- New.No.036/routes/user.js
+++ New.No.036/routes/user.js
@@ -1,8 +1,69 @@
+const Cryptojs = require("crypto-js");
+const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
-router.post("/regist", (req, res) => {
-  console.log(req.body);
-  res.end();
+const { User } = require("../models/index.js");
+
+router.post("/regist", async (req, res) => {
+  try {
+    const tempUser = await User.findOne({ where: { userId: req.body.id } });
+    if (tempUser) {
+      res.status(500);
+      res.send({ message: "exist ID" });
+      return;
+    }
+
+    const { id, pw, name, className } = req.body;
+    await User.create({
+      userId: id,
+      userPw: Cryptojs.SHA256(pw).toString(),
+      name,
+      class: className,
+    });
+    res.end();
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const tempUser = await User.findOne({ where: { userId: req.body.id } });
+    if (!tempUser) {
+      res.status(500);
+      res.send({ message: "no ID" });
+      return;
+    }
+
+    if (tempUser.userPw == Cryptojs.SHA256(req.body.pw).toString()) {
+      res.cookie(
+        "sid",
+        jwt.sign(
+          {
+            name: tempUser.name,
+          },
+          process.env.JWT_KEY,
+          {
+            algorithm: "HS256",
+            expiresIn: "30m",
+            issuer: "jkh",
+          }
+        )
+      );
+      res.send({
+        name: tempUser.name,
+      });
+      return;
+    }
+    res.status(500);
+    res.send({ message: "wrong password" });
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+});
+
+router.get("/logout", async (req, res) => {});
 
 module.exports = router;
