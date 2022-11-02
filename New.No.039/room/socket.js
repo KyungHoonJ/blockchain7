11f601a58fd4c06e0a47092e18d373b8739265eb
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-11-02 14:12:14 +0900

    39일차 추가

--- New.No.039/room/socket.js
+++ New.No.039/room/socket.js
@@ -3,25 +3,35 @@ const socket = require("socket.io");
 module.exports = (server) => {
   const io = socket(server);
 
-  // namespace << 라우터
-  // 진짜 라우터와 같은 역활을 한다.
-  // 소켓에 있어서 제일 큰 분류이다.
-  // axios.post('/api/board') => app.post('/api/board')
-  // socket('/room') => io.on('/room')
-  // socket('/chat') => io.on('/chat')
-  // socket('/user') => io.on('/user')
-  // 단점 : 연결이 따로 되는 것이기 때문에 ws 자체가 다르다.
-
-  // room << 방
-  // 이름 그대로 방이다.
-  // namespace의 하위에 속한다.
-  // io.on 이후에 사용된다. => ws를 마음껏 사용할 수 있다?
-
-  // on << 이벤트
-  // io.on / ws.on << 이 메서드들은 어떤 메서드일까? 어떤 상황일 때 실행되는 코드 << 콜백함수, addEventListener 같은 역활이다.
-  //   여러개 넣으면 여러번 실행된다. onclick = function과 addEventListener('click', function)과 비교하면서 테스트 해보고 addEventListener와 on은 같은 기능을 한다.
-
+  io.of("/test").on("connection", (ws) => {
+    // test라는 네임스페이스를 사용한다.
+    ws.on("test", (data) => {
+      console.log("/test", data);
+      console.log(ws.userId);
+    });
+  });
+  {
+    // namespace << 라우터
+    // 진짜 라우터와 같은 역활을 한다.
+    // 소켓에 있어서 제일 큰 분류이다.
+    // axios.post('/api/board') => app.post('/api/board')
+    // socket('/room') => io.on('/room')
+    // socket('/chat') => io.on('/chat')
+    // socket('/user') => io.on('/user')
+    // 단점 : 연결이 따로 되는 것이기 때문에 ws 자체가 다르다.
+    // room << 방
+    // 이름 그대로 방이다.
+    // namespace의 하위에 속한다.
+    // io.on 이후에 사용된다. => ws를 마음껏 사용할 수 있다?
+    // on << 이벤트
+    // io.on / ws.on << 이 메서드들은 어떤 메서드일까? 어떤 상황일 때 실행되는 코드 << 콜백함수, addEventListener 같은 역활이다.
+    //   여러개 넣으면 여러번 실행된다. onclick = function과 addEventListener('click', function)과 비교하면서 테스트 해보고 addEventListener와 on은 같은 기능을 한다.
+  }
   io.on("connection", (ws) => {
+    ws.on("test", (data) => {
+      console.log("/", data);
+    });
+
     ws.on("login", (data) => {
       ws.userId = data.id;
       ws.emit("login", { id: ws.userId });
