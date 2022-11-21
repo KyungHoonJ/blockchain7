04072ee672c696df130f54821eb02db3fffc6f9f
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2022-11-21 09:43:56 +0900

    클래스 VS 함수 state

--- New.No.042/testing/src/components/ClassComp.jsx
+++ New.No.042/testing/src/components/ClassComp.jsx
@@ -4,9 +4,18 @@ export default class ClassComp extends React.Component {
   constructor(props) {
     super(props);
     this.props.func();
+    this.state = { test: "state test" }; // state 선언 및 정의(초기화)
   }
 
   render() {
-    return <div>ClassComp {this.props.text}</div>;
+    return (
+      <div
+        onClick={function () {
+          this.setState({ test: this.state.test + "1" }); // state 재정의
+        }.bind(this)}
+      >
+        ClassComp {this.props.text} {this.state.test}
+      </div>
+    );
   }
 }
--- New.No.042/testing/src/components/FuncComp.jsx
+++ New.No.042/testing/src/components/FuncComp.jsx
@@ -1,12 +1,22 @@
 // export default function FuncComp(props) {
 //   const {text, func} = props
 //   props.func();
-
 //   return <div>FuncComp {props.text}</div>;
 // }
 
+import { useState } from "react";
+
 export default function FuncComp({ text, func }) {
+  const [test, setTest] = useState("state test"); // state 선언 및 정의(초기화)
   func();
 
-  return <div>FuncComp {text}</div>;
+  return (
+    <div
+      onClick={function () {
+        setTest(test + "1"); // state 재정의
+      }}
+    >
+      FuncComp {text} {test}
+    </div>
+  );
 }
