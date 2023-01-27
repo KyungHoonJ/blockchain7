a16639bdb08e0bed329be66670421109537a7d4b
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-01-27 18:40:30 +0900

    70일차 테스트 내용 추가

--- New.No.070/blockchain/src/index.ts
+++ New.No.070/blockchain/src/index.ts
@@ -69,6 +69,28 @@ const ports = [
   [8081, 7546],
 ];
 const idx = 0; // 테스트용
+// 1. idx를 0으로 정의한 후에 ts-node src/index 실행
+//   - 8080 HTTP 서버와 7545 Socket 서버가 배포된다.(실행된다, 열린다.)
+// 2. idx를 1로 정의한 후에 ts-node src/index 실행
+//   - 8081 HTTP 서버와 7546 Socket 서버가 배포된다.(실행된다, 열린다.)
+// 3. postman에서 테스트를 진행한다.
+//   - chains 테스트 : http://localhost:8080/chains 또는 http://localhost:8081/chains 주소에 GET 메서드를 사용한다.
+//     - 아무 것도 하지 않았다면 두 체인의 제네시스 블록이 다르게 뜬다.
+//     - POST 메서드에 /peer/add 라우터로 보낸 적이 있다면 같은 제네시스 블록을 확인할 수 있다.
+//     - POST 메서드에 /peer/add 라우터로 보낸 직후라면 같은 체인이 확인된다.
+//   - peer 테스트 : http://localhost:8080/peer 또는 http://localhost:8081/peer 주소에 GET 메서드를 사용한다.
+//     - 아무 것도 하지 않았다면 빈 배열이 뜬다.
+//     - POST 메서드에 /peer/add 라우터로 보낸 적이 있다면 보낸 횟수만큼 서로의 주소가 배열의 아이템으로 확인된다.
+//       - 2번 보냈다면 2개씩 존재한다.
+//   - block/mine 테스트 : http://localhost:8080/block/mine 또는 http://localhost:8081/block/mine 주소에 POST 메서드를 사용, body에 JSON 형식으로 "data"를 ["원하는 내용"]으로 보낸다.
+//     - 보낸 주소의 chains 테스트에서 블록이 추가됨이 확인된다.
+//     - 보내지 않은 주소에서는 블록이 추가되지 않음이 확인된다.
+//     - 8080 포트로 /block/mine 테스트를 실행했다면 8081 포트에서는 추가되지 않는 것이 확인되어야한다.
+//   - peer/add 테스트 : http://localhost:8080/peer/add 또는 http://localhost:8081/peer/add 주소에 POST 메서드를 사용, body에 JSON 형식으로 "peer"를 "ws://내주소:내포트"으로 보낸다.
+//     - 보내는 주소는 상대방의 주소이다.
+//     - 8080 기준에서 8081에 보낸다고 생각했을 때 주소는 http://localhost:8081/peer/add 이고 JSON 데이터는 {"peer":"ws://localhost:7545"} 이다.
+//     - 8081 기준에서 8080에 보낸다고 생각했을 때 주소는 http://localhost:8080/peer/add 이고 JSON 데이터는 {"peer":"ws://localhost:7546"} 이다.
+//     - 보낸 직후에 chains 테스트를 진행하여 chains를 확인하면 두 체인이 동기화되어 같아짐을 확인할 수 있다.
 
 app.listen(ports[idx][0], () => {
   console.log("server start " + ports[idx][0]);
