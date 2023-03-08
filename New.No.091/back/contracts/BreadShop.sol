8a3c17b2e7705512670087e1dc430b5a7a3173cb
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-03-08 14:23:36 +0900

    91일차 추가

--- New.No.091/back/contracts/BreadShop.sol
+++ New.No.091/back/contracts/BreadShop.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+// 빵 가격 정의, 컨트렉트 등록자는 수정 가능
+// 구매하는 빵 개수에 따라 자동으로 사용되는 Ether 정의
+// 구매할 때와 판매할 때 가격 변동 => 구매보다 판매할때 무조건 싸야함 << 남는 돈은 스마트 컨트렉트가 꿀꺽
+// - 해보고 싶은 사람 해보자 성진이는 필수
+//   - 따로 풀이 없음, 질문은 받음
+
 contract BreadShop {
   mapping(address => uint) public breads;
 
@@ -12,9 +18,9 @@ contract BreadShop {
     breads[msg.sender] += 1;
   }
 
-  function sellBread(uint amount) public payable {
+  function sellBread() public payable {
     breads[msg.sender] -= 1;
-    payable(msg.sender).transfer(amount * 10 ** 18);
+    payable(msg.sender).transfer(10 ** 18);
   }
 
   function getBread() public view returns (uint) {
