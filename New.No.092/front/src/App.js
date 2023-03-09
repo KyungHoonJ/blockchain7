03f76710c016be89352354e4c741cfd9e043fe80
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-03-09 14:49:30 +0900

    92일차 추가

--- New.No.092/front/src/App.js
+++ New.No.092/front/src/App.js
@@ -1,31 +1,47 @@
-import { useEffect, useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import Web3 from "web3";
+import { useEffect } from "react";
 import JJJTokenContract from "./JJJToken.json";
 
 function App() {
-  const [web3, setWeb3] = useState();
-  const [account, setAcocunt] = useState();
-
   useEffect(() => {
     (async () => {
-      const _web3 = new Web3("http://ganache.test.errorcode.help:8545");
-      setWeb3(_web3);
+      const web3 = new Web3(window.ethereum);
+      const contract = new web3.eth.Contract(JJJTokenContract.abi);
       const [_account] = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
-      setAcocunt(_account);
-
-      console.log(_account);
-      const { abi, bytecode } = JJJTokenContract;
-      const deployer = new _web3.eth.Contract(abi);
-      const deployed = await deployer
-        .deploy({ data: bytecode })
-        .send({ from: _account });
+      const txObj = {
+        data: JJJTokenContract.bytecode,
+        arguments: ["JJJToken", "JJJTK11", 10000],
+      };
+      const deployed = await contract.deploy(txObj).send({
+        from: _account,
+        gas: 2000000,
+      });
       console.log(deployed.options.address);
     })();
   }, []);
-  return <div className="App"></div>;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
